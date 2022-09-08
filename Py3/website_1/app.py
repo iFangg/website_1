9a27bf19cb5ac3ff6e3d66895d1310bccb190248
iXhaha <iFang.py@gmail.com>
2022-09-08 13:40:50 +1000

    Finishing up forms and user input

--- Py3/website_1/app.py
+++ Py3/website_1/app.py
@@ -30,9 +30,15 @@ def home():
 def about():
     return(render_template('about.html'))
 
-@app.route("/login")
+@app.route("/login", methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash("Successfully signed in!", 'Success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return(render_template('login.html', title = 'Login', form = form))
 
 @app.route("/register", methods = ['GET', 'POST'])
--- Py3/website_1/forms.py
+++ Py3/website_1/forms.py
@@ -5,10 +5,10 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min = 2)])
+    username = StringField('Username', validators=[DataRequired(), Length(min = 2, max = 20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('Password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
 
